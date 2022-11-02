1a8d74c6f51d1f4fa986b2a66dd55dfefa8fc7c6
tugba74 <tugba8374@gmail.com>
2022-11-02 17:34:57 +0300

    aciklamalar eklendi

--- src/A04.java
+++ src/A04.java
@@ -1,5 +1,17 @@
 public class A04 {
     public static void main(String[] args) {
+/*
+bir projeyi ilk defa olusturdumuzda
+1-commit olustur
+2- uuzak masaüstünde bir repo olustur
+3-github da istenen son 3 satiri copy yzpip
+projeye paste yapıp çalıştır.
 
+bir projeyi bir kere uzak masağstğ ie ilişkilendirince
+sonra yapacağım her değişiklik için
+1-git add . : yeni yapılan değişikliklşkleri git e ekler
+2-git commit -m"istenen aciklama" : degisikleri isleyerek versyon olusturur
+3-git push :son versiyonu uzak bir masaüstünde yollar
+ */
     }
 }
